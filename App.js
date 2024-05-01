9b6811f648b4918ca92470d2de94b8abc94c7dbe
bhavesh07 <bhavesh_purswani@momentumventures.io>
2024-05-01 11:48:24 +0530

    dom manipulation done manually but by react

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = [
+  React.createElement(
+    "div",
+    { id: "parent1" },
+    React.createElement("h1", { id: "child" }, "hello world")
+  ),
+  React.createElement("h2", { id: "parent2" }, [
+    React.createElement("h2", { id: "child1" }, "hello world2"),
+    React.createElement("h3", { id: "child2" }, "hello world3"),
+  ]),
+];
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
