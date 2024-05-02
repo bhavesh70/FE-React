cf49a5eac1bc8291ee72fe3e1a906e7fbdb57246
bhavesh07 <bhavesh_purswani@momentumventures.io>
2024-05-02 12:30:36 +0530

    jsx basics is done with babble and parcel core concept understanding + function component basics and component composition

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// React.createElement("h1", { id: "child" }, "hello world 1")  ==> React Element ==> JS Object ==> HtmlElement
 const heading = [
   React.createElement(
     "div",
@@ -12,7 +13,37 @@ const heading = [
     React.createElement("h3", { id: "child2" }, "hello world3"),
   ]),
 ];
+
+// JSX ==> Babale transpiles into React.createElement("h1", { id: "child" }, "hello world 1")  ==> React Element ==> JS Object ==> HtmlElement(render)
+
+const jsxHeading = (
+  <div id="jsxDiv">
+    <h1 className="jsxChild" id="jsxId">
+      Hello world
+    </h1>
+  </div>
+); // Jsx element
+
+const Title = () => <h1 className="title">Hello from functional component</h1>;
+
+// Functional component
+const HeadingComponent = () => (
+  <div id="jsxDiv">
+    <Title />
+    <h2 className="jsxChild" id="jsxId">
+      Hello world
+    </h2>
+  </div>
+);
+
+// const HeadingComponent = () => (
+//     <div id="jsxDiv">
+//       <Title />    <------------------------------ component composition composing component inside component
+//       <h2 className="jsxChild" id="jsxId">
+//         Hello world
+//       </h2>
+//     </div>
+//   );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-
+root.render(<HeadingComponent />);
